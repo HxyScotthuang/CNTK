77710809ef6f69bad670a9bd1e44735f48cd0cc9
Xingyue Huang <49787534+HxyScotthuang@users.noreply.github.com>
2023-02-21 12:23:09 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ def load_cifar(path = "cifar-10-batches-py"):
 	train_labels = []
 
 	for i in range(1, 6):
-		cifar_out = pickle.load(open(os.path.join(path, "data_batch_{0}".format(i))),encoding = 'iso-8859-1')
+		cifar_out = pickle.load(open(os.path.join(path, "data_batch_{0}".format(i)),'rb'),encoding = 'iso-8859-1')
 		train_batches.append(cifar_out[b"data"])
 		train_labels.extend(cifar_out[b"labels"])
 	X_train= np.vstack(tuple(train_batches)).reshape(-1, 3, 32, 32)
 	y_train = np.array(train_labels)
 
-	cifar_out = pickle.load(open(os.path.join(path, "test_batch")),encoding = 'iso-8859-1')
+	cifar_out = pickle.load(open(os.path.join(path, "test_batch"),'rb'),encoding = 'iso-8859-1')
 	X_test = cifar_out[b"data"].reshape(-1, 3, 32, 32)
 	y_test = cifar_out[b"labels"]
 	
